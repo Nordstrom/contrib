4e3246aaf87ac0f05c34d503f8897f06e03bfecf
Emmanuel Gomez <emmanuel@gomez.io>
2016-02-18 17:24:23 -0800

    Add support for passing config args (API server URL, config file path).

--- Ingress/controllers/nginx-alpha/controller.go
+++ Ingress/controllers/nginx-alpha/controller.go
@@ -17,18 +17,29 @@ limitations under the License.
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"os/exec"
 	"reflect"
 	"text/template"
 
+	"github.com/golang/glog"
+
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
+	clientcmd "k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/util"
+)
+
+var (
+	argKubecfgFile   = flag.String("kubecfg-file", "", "Location of kubecfg file for access to kubernetes API service; --kube-apiserver-url overrides the URL part of this; if neither this nor --kube-apiserver-url are provided, defaults to service account tokens")
+	argKubeMasterURL = flag.String("kube-apiserver-url", "", "URL to reach kubernetes API server. Env variables in this flag will be expanded.")
 )
 
 const (
@@ -83,11 +94,15 @@ func shellOut(cmd string) {
 }
 
 func main() {
-	var ingClient client.IngressInterface
-	if kubeClient, err := client.NewInCluster(); err != nil {
-		log.Fatalf("Failed to create client: %v.", err)
-	} else {
-		ingClient = kubeClient.Extensions().Ingress(api.NamespaceAll)
+	var (
+		kubeClient       *client.Client
+		kubeClientConfig *client.Config
+		err              error
+	)
+	if kubeClientConfig, err = newKubeClientConfig(); err != nil {
+		log.Fatalf("Failed to create client config: %v.", err)
+	} else if kubeClient, err = client.New(kubeClientConfig); err != nil {
+		log.Fatalf("Failed to create kubernetes client: %v.", err)
 	}
 	tmpl, _ := template.New("nginx").Parse(nginxConf)
 	rateLimiter := util.NewTokenBucketRateLimiter(0.1, 1)
@@ -113,4 +128,57 @@ func main() {
 		}
 		shellOut("nginx -s reload")
 	}
+}
+
+// Lightly adapted from kubernetes/cluster/addons/dns/kube2sky/kube2sky.go
+func newKubeClientConfig() (*client.Config, error) {
+	var (
+		config       *client.Config
+		err          error
+		apiServerURL string
+	)
+	// If the user specified --kube-apiserver-url, expand env vars and verify it.
+	if *argKubeMasterURL != "" {
+		apiServerURL, err = expandKubeMasterURL()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if apiServerURL != "" && *argKubecfgFile == "" {
+		// Only --kube-apiserver-url was provided.
+		config = &client.Config{
+			Host:    apiServerURL,
+			Version: "v1",
+		}
+	} else {
+		// We either have:
+		//  1) --kube-apiserver-url and --kubecfg-file
+		//  2) just --kubecfg-file
+		//  3) neither flag
+		// In any case, the logic is the same.  If (3), this will automatically
+		// fall back on the service account token.
+		overrides := &clientcmd.ConfigOverrides{}
+		overrides.ClusterInfo.Server = apiServerURL                                 // might be "", but that is OK
+		rules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: *argKubecfgFile} // might be "", but that is OK
+		if config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides).ClientConfig(); err != nil {
+			return nil, err
+		}
+	}
+
+	glog.Infof("Using %s for kubernetes API server", config.Host)
+	glog.Infof("Using kubernetes API version %s", config.Version)
+
+	return config, nil
+}
+
+func expandKubeMasterURL() (string, error) {
+	parsedURL, err := url.Parse(os.ExpandEnv(*argKubeMasterURL))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse --kube-apiserver-url %s - %v", *argKubeMasterURL, err)
+	}
+	if parsedURL.Scheme == "" || parsedURL.Host == "" || parsedURL.Host == ":" {
+		return "", fmt.Errorf("invalid --kube-apiserver-url specified %s", *argKubeMasterURL)
+	}
+	return parsedURL.String(), nil
 }
